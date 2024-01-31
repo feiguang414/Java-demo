f64b34ca22c5407144380fdcaf61a0dbd1ffbdf9
feiguang414 <ljy_414@163.com>
2024-01-31 21:48:46 +0800

    字符串的format

---
+++ hsp_basic/src/com/ljy/house_/String_format_.java
@@ -0,0 +1,13 @@
+package com.ljy.house_;
+
+import java.util.Calendar;
+
+public class String_format_ {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        String s = String.format("Duke's Birthday: %1$tm %1$te,%1$tY", c);
+        System.out.println(s);
+        System.out.println(c);
+        String str = String.format()
+    }
+}
---
+++ notes/Java-api.md
@@ -0,0 +1,6 @@
+
+
+###### `static String--format(String format, Object... args)`
+
+返回一个formatted string，使用设定的格式和参数。
+
